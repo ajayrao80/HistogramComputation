5d17817b2b883c5138dcba3fd5115efbff30e39e
Ajay Rao <krrao.rao@gmail.com>
2021-10-28 22:07:33 +0530

    Update HistogramComputation.py

--- HistogramComputation.py
+++ HistogramComputation.py
@@ -1,40 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[12]:
-
-
 import cv2
 import math
 import matplotlib.pyplot as plt
 
 
-# In[26]:
-
-
-img = cv2.imread("sample.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  
-cv2.imshow("Image", img)
-cv2.waitKey()
-cv2.destroyAllWindows()
-
-
-# In[75]:
-
-
-# show the plotting graph of an image using built in function
-histr = cv2.calcHist([img],[0],None,[256],[0,256])
-
-histr = [histr[i][0] for i in range(256)]
-#plt.plot(histr)
-plt.bar([i for i in range(1, 257)], histr)
-plt.show()
-
-
-# In[77]:
-
-
+# Function to calculate histogram
 def calcHist(img):
     hist = [[i*0] for i in range(256)]
     height, width = img.shape
@@ -42,12 +11,27 @@ def calcHist(img):
         for j in range(width):
             hist[img[i][j]][0] += 1
     
-    return hist    
+    return hist  
+
+  
+
+# Driver code
+img = cv2.imread("sample.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Image", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# show the plotting graph of an image using built in function
+histr = cv2.calcHist([img],[0],None,[256],[0,256])
+histr = [histr[i][0] for i in range(256)]
+#plt.plot(histr)
+plt.bar([i for i in range(1, 257)], histr)
+plt.show()
 
 
-# In[86]:
 
-
+# Plotting histogram
 hist = calcHist(img)
 hist = [hist[i][0] for i in range(256)]
 #plt.plot(hist)
@@ -55,7 +39,9 @@ plt.bar([i for i in range(1, 257)], hist)
 plt.show()
 
 
-# In[ ]:
+
+
+
 
 
 
